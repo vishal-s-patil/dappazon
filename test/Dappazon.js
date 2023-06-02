7741117a41eaf115d41c3d3d94922c916d91b9de
vishalspatil <vishalspolicepatil55@gmail.com>
2023-06-02 20:01:06 +0530

    first test

--- test/Dappazon.js
+++ test/Dappazon.js
@@ -1,9 +1,23 @@
 const { expect } = require("chai")
+const { ethers } = require("hardhat")
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
+	return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
 describe("Dappazon", () => {
+	let dappazon;
 
+	beforeEach(async ()=> {
+		const Dappazon = await ethers.getContractFactory("Dappazon")
+		dappazon = await Dappazon.deploy()
+	})
+
+	describe("Deployment", () => {
+		it('has a name', async () => {
+			expect(await dappazon.name()).to.equal("Dappazon");
+		})
+	})
+
+	
 })
